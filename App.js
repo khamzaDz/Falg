85cb87dd759eb83cfaa9efe93ff46d047ae6190e
khamza <khamzadzeit00@gmail.com>
2024-01-23 14:06:36 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./screens/HomeScreen";
+import { StatusBar } from "react-native";
+import colors from "./assets/colors/colors";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+// const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <>
+      <StatusBar barStyle={"dark-content"} />
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Главное"
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused }) => {
+              let iconName;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+              if (route.name === "Главное") {
+                iconName = focused ? "home" : "home-outline";
+              } else if (route.name === "Избранное") {
+                iconName = focused ? "heart" : "heart-outline";
+              } else if (route.name === "Профиль") {
+                iconName = focused ? "person" : "person-outline";
+              }
+
+              return (
+                <Ionicons
+                  name={iconName}
+                  size={26}
+                  color={focused ? colors.kaka : colors.grey}
+                />
+              );
+            },
+            tabBarActiveTintColor: colors.kaka,
+            tabBarInactiveTintColor: colors.grey,
+            headerShown: false,
+          })}
+        >
+          <Tab.Screen name="Главное" component={HomeScreen} />
+          <Tab.Screen name="Избранное" component={HomeScreen} />
+          <Tab.Screen name="Профиль" component={HomeScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
